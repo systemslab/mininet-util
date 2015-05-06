5cfcda3353122370a527e821526a14eb309a6782
Andrew Shewmaker <agshew@gmail.com>
2015-05-06 11:00:44 -0700

    srtt plot speedups

--- plot_tcpprobe.R
+++ plot_tcpprobe.R
@@ -1,4 +1,6 @@
 #!/usr/bin/env Rscript
+require(data.table)
+require(hexbin)
 
 argv <- commandArgs(TRUE)
 
@@ -13,118 +15,117 @@ if (length(argv) < 1) {
 
 t <- list()
 srtts <- list()
+hbins <- list()
 
 d1name <- argv[1]
-d1 <- read.table(d1name, sep=" ", header=F)
+d1 <- fread(d1name)
 d1tshifted <- d1$V1 - min(d1$V1)
 t$d1 <- d1tshifted
 maxt <- max(d1tshifted)
 srtts$d1 <- d1$V10
+hbinsd1 <- hexbin(d1tshifted, d1$V10, xbins=40)
 srtt_min <- min(d1$V10)
 srtt_max <- max(d1$V10)
+srtt_max99th <- quantile(d1$V10, c(0.99))
+srtt_min1st <- quantile(d1$V10, c(0.01))
 
 if (length(argv) > 1) {
 	d2name <- argv[2]
-	d2 <- read.table(d2name, sep=" ", header=F)
+	d2 <- fread(d2name)
 	d2tshifted <- d2$V1 - min(d2$V1)
 	t$d2 <- d2tshifted
 	maxt <- max(maxt, d2tshifted)
 	srtts$d2 <- d2$V10
 	srtt_max <- max(srtt_max, d2$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d2$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d2$V10, c(0.01)))
 }
 
 if (length(argv) > 2) {
 	d3name <- argv[3]
-	d3 <- read.table(d3name, sep=" ", header=F)
+	d3 <- fread(d3name)
 	d3tshifted <- d3$V1 - min(d3$V1)
 	maxt <- max(maxt, d3tshifted)
 	t$d3 <- d3tshifted
 	srtts$d3 <- d3$V10
 	srtt_max <- max(srtt_max, d3$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d3$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d3$V10, c(0.01)))
 }
 
 if (length(argv) > 3) {
 	d4name <- argv[4]
-	d4 <- read.table(d4name, sep=" ", header=F)
+	d4 <- fread(d4name)
 	d4tshifted <- d4$V1 - min(d4$V1)
 	maxt <- max(maxt, d4tshifted)
 	t$d4 <- d4tshifted
 	srtts$d4 <- d4$V10
 	srtt_max <- max(srtt_max, d4$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d4$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d4$V10, c(0.01)))
 }
 
 if (length(argv) > 4) {
 	d5name <- argv[5]
-	d5 <- read.table(d5name, sep=" ", header=F)
+	d5 <- fread(d5name)
 	d5tshifted <- d5$V1 - min(d5$V1)
 	maxt <- max(maxt, d5tshifted)
 	t$d5 <- d5tshifted
 	srtts$d5 <- d5$V10
 	srtt_max <- max(srtt_max, d5$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d5$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d5$V10, c(0.01)))
 }
 
 if (length(argv) > 5) {
 	d6name <- argv[6]
-	d6 <- read.table(d6name, sep=" ", header=F)
+	d6 <- fread(d6name)
 	d6tshifted <- d6$V1 - min(d6$V1)
 	maxt <- max(maxt, d6tshifted)
 	t$d6 <- d6tshifted
 	srtts$d6 <- d6$V10
 	srtt_max <- max(srtt_max, d6$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d6$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d6$V10, c(0.01)))
 }
 
 if (length(argv) > 6) {
 	d7name <- argv[7]
-	d7 <- read.table(d7name, sep=" ", header=F)
+	d7 <- fread(d7name)
 	d7tshifted <- d7$V1 - min(d7$V1)
 	maxt <- max(maxt, d7tshifted)
 	t$d7 <- d7tshifted
 	srtts$d7 <- d7$V10
 	srtt_max <- max(srtt_max, d7$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d7$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d7$V10, c(0.01)))
 }
 
 if (length(argv) > 7) {
 	d8name <- argv[8]
-	d8 <- read.table(d8name, sep=" ", header=F)
+	d8 <- fread(d8name)
 	d8tshifted <- d8$V1 - min(d8$V1)
 	maxt <- max(maxt, d7tshifted)
 	t$d8 <- d8tshifted
 	srtts$d8 <- d8$V10
 	srtt_max <- max(srtt_max, d8$V10)
+	srtt_max99th <- max(srtt_max99th, quantile(d8$V10, c(0.99)))
+	srtt_min1st <- min(srtt_min1st, quantile(d8$V10, c(0.01)))
 }
 
 png(file="srtt.png", height=900, width=1500, pointsize=12)
 
-colors <- c("red", "darkolivegreen3", "cornflowerblue", "plum4", "darksalmon", "aquamarine", "darkgoldenrod1", "black")
-
-# Trim off excess margin space (bottom, left, top, right)
-#par(mar=c(4.2, 3.8, 0.2, 0.2))
-
-plot(d1tshifted, d1$V10, type="o", col=colors[1], lty=1, pch=1, lwd=1, cex=0.5,
-	axes=F, ann=T, xlab="Time (s)", ylab="Smoothed RTT (us)",
-	log="y", xlim=c(0, min(maxt, max(d1tshifted))), ylim=c(max(1, srtt_min), srtt_max))
-
-for (i in 2:length(argv)) {
-	lines(unlist(t[i]), unlist(srtts[i]), type="o", lty=i, pch=i, lw=1, cex=0.5, col=colors[i])
-}
-
-#abline(h=maxrate, col="gray60")
-#text(1, maxrate+0.15, format(maxrate, digits=2))
+plot(hbinsd1, xlab="Time (s)", ylab="Smoothed RTT (us)", legend=1.5)
  
-axis(1, las=1, cex.axis=1.0)
-axis(2, las=1, cex.axis=1.0)
-
-ltys <- rep(1:8)
-pchs <- rep(1:8)
-legend("left", argv, cex=1.0,
-	col=colors[1:length(argv)], 
-	lty=ltys[1:length(argv)], pch=pchs[1:length(argv)], lwd=3, bty="n");
-  
 # Turn off device driver (to flush output)
 dev.off()
 
+colors <- c("red", "darkolivegreen3", "cornflowerblue", "plum4", "darksalmon", "aquamarine", "darkgoldenrod1", "black")
+ltys <- rep(1:8)
+pchs <- rep(1:8)
+
 png(file="srtt-cdf.png", height=800, width=800, pointsize=12)
-plot(0, 0, xlab="Smoothed RTT (us)", ylab="Probability", xlim=c(max(1, srtt_min), srtt_max), ylim=c(0, 1),
+plot(1, 1, xlab="Smoothed RTT (us)", ylab="Probability", xlim=c(max(1, srtt_min1st), srtt_max99th), ylim=c(0, 1),
 	log="x", type="n", axes=F, ann=T)
 for (i in 1:length(argv)) {
 	lines(ecdf(unlist(srtts[i])), lty=i, pch=i, lw=2, col=colors[i])
