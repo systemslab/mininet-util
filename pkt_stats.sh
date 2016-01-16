c53d1458b839e1eaad6351fc3a620729334fcf0c
Andrew Shewmaker <shewa@lanl.gov>
2016-01-16 05:27:31 -0800

    add fast retransmit stats

--- pkt_stats.sh
+++ pkt_stats.sh
@@ -2,28 +2,29 @@
 
 # TODO: rewrite as actual perl script
 
-#echo "segments_sent_out segments_retransmitted percent_retransmitted retransmitted_lost percent_retransmitted_lost packets_switch_out packets_switch_dropped percent_dropped"
-echo -e "seg_tx\tretx\t%retex\trelost\t%relost\tpkt_sw\tdropped\t%dropped"
+#echo "segments_sent_out segments_retransmitted percent_retransmitted fast_retransmits percent_fast_retransmits lost_retransmits percent_lost_retransmits packets_queued_switch packets_dropped_switch percent_dropped"
+echo -e "segTX\treTX\t%reTX\tfasReTX\t%fasReTX\tlReTX\t%lReTX\tpktsQd\tdrop\t%drop"
 
 files=$(ls $1/netstat-h[2-9]*.txt $1/netstat-h[2-9]*-after.txt 2> /dev/null)
 script=$(cat <<'RETX'
 BEGIN {
   my $sent = 0;
   my $retrans = 0;
+  my $fastretrans = 0;
   my $retrans_lost = 0;
 }
 
 /(\d+) segments send out/ && ($sent = $sent + $1);
 /(\d+) segments retransmited/ && ($retrans = $retrans + $1);
+/(\d+) fast retransmits/ && ($fastretrans = $fastretrans + $1);
 /TCPLostRetransmit: (\d+)/ && ($retrans_lost = $retrans_lost + $1);
 
 END {
   if($sent == 0) {
-    printf("NA\tNA\tNA\tNA\tNA\t");
+    printf("NA\tNA\tNA\tNA\tNA\tNA\t");
     exit;
   }
 
-  my $percent_retrans = 100 * $retrans / $sent;
   if($retrans == 0) {
     printf("%d\t%d\t%3.4f\t%d\t%3.4f\t", $sent, 0, 0.0, 0, 0.0);
     exit;
@@ -31,13 +32,25 @@ END {
     printf("%d\t%d\t", $sent, $retrans);
   }
 
+  my $percent_retrans = 100 * $retrans / $sent;
   if($percent_retrans < 0.0001) {
     print "> 0\t";
   } else {
     printf("%3.4f\t", $percent_retrans);
   }
 
-  my $percent_retrans_lost = 100 * $retrans_lost / $retrans;
+  if($fastretrans == 0) {
+    printf("%d\t%3.4f\t", 0, 0.0);
+  } else {
+    printf("%d\t", $fastretrans);
+    my $percent_fastretrans = 100 * $fastretrans / $retrans;
+    if($percent_fastretrans < 0.0001) {
+      print "> 0\t";
+    } else {
+      printf("%3.4f\t", $percent_fastretrans);
+    }
+  }
+
   if($retrans_lost == 0) {
     printf("%d\t%3.4f\t", 0, 0.0);
     exit;
@@ -45,6 +58,7 @@ END {
     printf("%d\t", $retrans_lost);
   }
 
+  my $percent_retrans_lost = 100 * $retrans_lost / $retrans;
   if ($retrans_lost > 0 && $percent_retrans_lost < 0.0001) {
     print "> 0\t";
   } else {
